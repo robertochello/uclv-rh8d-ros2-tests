82f504c906c2cdf343f888fc9ead8635cd223d15
robertochello <roberto.chello@studenti.unicampania.it>
2024-05-31 20:52:41 +0200

    created test_moveMotors.cpp test_readMotorsPositions.cpp

---
+++ src/test_moveMotors.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "custom_msg/msg/position.hpp"
+
+#include "my_library/hand.hpp"
+
+
+
+
+class TestMoveMotors : public rclcpp::Node
+{
+public:
+
+    std::shared_ptr<Hand> hand_;
+    dynamixel::PortHandler *portHandler;
+    dynamixel::PacketHandler *packetHandler;
+
+    std::string serial_port_ = "/dev/ttyUSB0";
+    int baudrate_ = 1000000;
+    float protocol_version_ = 2.0;
+
+    rclcpp::Subscription<custom_msg::msg::Position>::SharedPtr subscription_;
+
+
+    TestMoveMotors()
+    : Node("test_move_motors_node")
+    {
+        hand_ = std::make_shared<Hand>(serial_port_, baudrate_, protocol_version_);
+        hand_->setSerialPortLowLatency(serial_port_);
+        if(!hand_->initialize()) {
+            throw std::runtime_error("Error: Hand not initialized");
+        }
+        
+        subscription_ = this->create_subscription<custom_msg::msg::Position>(
+                "/cmd/motor_position", 1,
+                std::bind(&TestMoveMotors::topic_callback, this, std::placeholders::_1)
+            );
+    }
+
+private:
+    void topic_callback(const custom_msg::msg::Position::SharedPtr pos) {
+        try {
+            hand_->moveMotors(pos->ids, pos->positions);
+        }
+        catch(...) {
+            RCLCPP_INFO_STREAM_THROTTLE(this->get_logger(), *this->get_clock(), 1000, "ERROR");
+            return;
+        }        
+    }
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    try {
+        auto hand_driver_node = std::make_shared<TestMoveMotors>();
+        rclcpp::spin(hand_driver_node);
+    } catch (const std::exception &e) {
+        RCLCPP_FATAL(rclcpp::get_logger("rclcpp"), "Exception caught: %s", e.what());
+        rclcpp::shutdown();
+        return 1;
+    }
+    rclcpp::shutdown();
+    return 0;
+}
